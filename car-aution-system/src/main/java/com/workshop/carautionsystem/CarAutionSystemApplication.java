cbaaa32c62b6541bcfc1af44207fcda0289e5f0f
khangnvhe141619 <97073195+khangnvhe141619@users.noreply.github.com>
2022-09-29 00:46:58 +0700

    init

---
+++ car-aution-system/src/main/java/com/workshop/carautionsystem/CarAutionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.carautionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAutionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAutionSystemApplication.class, args);
+	}
+
+}
