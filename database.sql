d0d5bd7026465ebe5b39f732c9427f875cd498c8
SyNV2 <92910131+SyNVHe141716@users.noreply.github.com>
2022-10-04 20:17:14 +0700

    Update database.sql
    update sql

--- database.sql
+++ database.sql
@@ -1,87 +1,86 @@
 --CREATE DATABASE SWP490_CarAutionSystem;
-
-CREATE TABLE IF NOT EXISTS User(
-    ID INT auto_increment primary key,
-    UserName VARCHAR(255) NULL,
-    Password VARCHAR(255) NOT NULL,
-    LName VARCHAR(255) NOT NULL,
-    FName VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL,
-    Phone INT NOT NULL,
-    RoleId INT NOT NULL,
-    CreatedDate DATETIME NOT NULL
+use SWP490_CarAutionSystem;
+CREATE TABLE `User`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `UserName` VARCHAR(255) NULL,
+    `Password` VARCHAR(255) NOT NULL,
+    `LName` VARCHAR(255) NOT NULL,
+    `FName` VARCHAR(255) NOT NULL,
+    `Email` VARCHAR(255) NOT NULL,
+    `Phone` INT NOT NULL,
+    `RoleId` INT NOT NULL
 );
 
-CREATE TABLE IF NOT exists Set_Bids(
-    Session_id INT auto_increment PRIMARY KEY,
-    Bidder_id INT,
-    Bids int NOT NULL
+CREATE TABLE `Set_Bids`(
+    `Session_id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `Bidder_id` INT UNSIGNED NOT NULL,
+    `Bids` DOUBLE(8, 2) NOT NULL
 );
 
-CREATE table IF NOT exists Role(
-    ID INT auto_increment PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL
+CREATE TABLE `Role`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `userId` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT exists Session(
-    ID INT auto_increment PRIMARY KEY,
-    Date DATE NOT NULL,
-    Start_time TIME NOT NULL,
-    End_Time TIME NOT NULL,
-    Winner_Id INT NOT NULL,
-    Invoice VARCHAR(255) NOT NULL
+CREATE TABLE `Session`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `date` DATE NOT NULL,
+    `Start_time` TIME NOT NULL,
+    `End_Time` TIME NOT NULL,
+    `Winner_Id` INT NOT NULL,
+    `Invoice` VARCHAR(255) NOT NULL
 );
 
-CREATE table IF NOT exists Notification(
-    ID INT auto_increment PRIMARY KEY,
-    Message VARCHAR(255) NOT NULL
+CREATE TABLE `User_Notification`(
+    `User_id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `Notification_id` INT UNSIGNED NOT NULL
 );
 
-CREATE table IF NOT exists User_Notification(
-    User_id INT NOT NULL ,
-    Notification_id INT NOT NULL
+CREATE TABLE `Notification`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `Message` VARCHAR(255) NOT NULL
 );
 
-CREATE table IF NOT exists Cartypes(
-    ID INT auto_increment PRIMARY KEY,
-    SeatCapacity INT NOT NULL,
-    Name VARCHAR(255) NOT NULL
+CREATE TABLE `Car`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `Name_car` VARCHAR(255) NOT NULL,
+    `Picture` VARCHAR(255) NOT NULL,
+    `Price` DOUBLE(8, 2) NOT NULL,
+    `model` VARCHAR(255) NOT NULL,
+    `yearOfmake` DATE NOT NULL,
+    `color` INT NOT NULL,
+    `cartype_id` INT UNSIGNED NOT NULL,
+    `category_id` INT UNSIGNED NOT NULL,
+    `session_id` INT UNSIGNED NOT NULL,
+    `Seller_id` INT UNSIGNED NOT NULL
 );
 
-CREATE table IF NOT exists Category_car(
-    ID INT auto_increment PRIMARY KEY,
-    Name_brand INT NOT NULL
+CREATE TABLE `cartypes`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `SeatCapacity` INT NOT NULL,
+    `Name` VARCHAR(255) NOT NULL
 );
 
-CREATE table IF NOT exists Car(
-    ID INT UNSIGNED auto_increment PRIMARY KEY,
-    Name_car VARCHAR(255) NOT NULL,
-    Picture VARCHAR(255) NOT NULL,
-    Price DOUBLE(8, 2) NOT NULL,
-    Model VARCHAR(255) NOT NULL,
-    YearOfMake DATE NOT NULL,
-    Color INT NOT NULL,
-    Cartype_id INT,
-    Category_id INT,
-    Session_id INT
+CREATE TABLE `Category_car`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `name_Brand` INT NOT NULL
 );
 
-CREATE table IF NOT exists Session_Participants(
-    Session_id INT UNSIGNED NOT NULL auto_increment PRIMARY KEY,
-    Bidder_id INT NOT NULL
+CREATE TABLE `Session_Participants`(
+    `session_id` INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+    `Bidder_id` INT NOT NULL
 );
 
-ALTER table `Set_Bids` 
-ADD CONSTRAINT `set_bids_bidder__id_foreign` FOREIGN KEY(`Bidder__id`) REFERENCES `User`(`ID`);
-
-ALTER table `User_Notification` 
-ADD CONSTRAINT `user_notification_notification_id_foreign` FOREIGN KEY(`Notification_id`) REFERENCES `Notification`(`ID`);
-
-ALTER table `Car` 
-ADD CONSTRAINT `car_session_id_foreign` FOREIGN KEY(`Session_id`) REFERENCES `Session`(`ID`);
-
-ALTER TABLE`Car` 
-ADD CONSTRAINT `car_category_id_foreign` FOREIGN KEY(`Category_id`) REFERENCES `Category_car`(`ID`);
-   
-ALTER TABLE `Car` 
-ADD CONSTRAINT `car_cartype_id_foreign` FOREIGN KEY(`Cartype_id`) REFERENCES `Cartypes`(`ID`);
+ALTER TABLE
+    `Set_Bids` ADD CONSTRAINT `set_bids_bidder_id_foreign` FOREIGN KEY(`Bidder_id`) REFERENCES `User`(`id`);
+ALTER TABLE
+    `Car` ADD CONSTRAINT `car_seller_id_foreign` FOREIGN KEY(`Seller_id`) REFERENCES `User`(`id`);
+ALTER TABLE
+    `User_Notification` ADD CONSTRAINT `user_notification_notification_id_foreign` FOREIGN KEY(`Notification_id`) REFERENCES `Notification`(`id`);
+ALTER TABLE
+    `Car` ADD CONSTRAINT `car_session_id_foreign` FOREIGN KEY(`session_id`) REFERENCES `Session`(`id`);
+ALTER TABLE
+    `Car` ADD CONSTRAINT `car_category_id_foreign` FOREIGN KEY(`category_id`) REFERENCES `Category_car`(`id`);
+ALTER TABLE
+    `Car` ADD CONSTRAINT `car_cartype_id_foreign` FOREIGN KEY(`cartype_id`) REFERENCES `cartypes`(`id`);
