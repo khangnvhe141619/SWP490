c09454cbf0c83f2087e14b43e3d74540a06c4bd4
Hoangdvhe140985 <77007481+Hoangdvhe140985@users.noreply.github.com>
2022-11-03 23:31:43 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `swp490_cab` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `swp490_cab`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: swp490_cab
