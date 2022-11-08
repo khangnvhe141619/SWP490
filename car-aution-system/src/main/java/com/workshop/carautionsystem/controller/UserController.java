dc42f97ab6694b9faea1a0dcedd5de51e6fe5414
khangnvhe141619 <97073195+khangnvhe141619@users.noreply.github.com>
2022-11-08 22:43:20 +0700

    port error

--- car-aution-system/src/main/java/com/workshop/carautionsystem/controller/UserController.java
+++ car-aution-system/src/main/java/com/workshop/carautionsystem/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.workshop.carautionsystem.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+@RestController
 public class UserController {
+    @GetMapping("/login")
+    public ModelAndView ridirect(){
+        ModelAndView view = new ModelAndView();
+        view.setViewName("Sign-In-Up");
+        return view;
+    }
 }
