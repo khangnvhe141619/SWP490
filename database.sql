43ff3f97634b3025b04ba4abb107f3fc6cd4f6a7
SyNV2 <92910131+SyNVHe141716@users.noreply.github.com>
2022-11-26 15:47:14 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -454,6 +454,39 @@ LOCK TABLES `roomparticipant` WRITE;
 /*!40000 ALTER TABLE `roomparticipant` ENABLE KEYS */;
 UNLOCK TABLES;
 
+
+--
+-- Table structure for table `roomdetailplayer`
+--
+
+DROP TABLE IF EXISTS `roomdetailplayer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roomdetailplayer` (
+	`id` int NOT NULL AUTO_INCREMENT,
+  `roomId` int NOT NULL,
+  `userId` int NOT NULL,
+  `userBid` int NOT NULL,
+  `timeBid` datetime NOT NULL,
+  `winner` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `roomdetailplayer_roomid_foreign` (`roomId`),
+  KEY `roomdetailplayer_userid_foreign` (`userId`),
+  CONSTRAINT `roomdetailplayer_roomid_foreign` FOREIGN KEY (`roomId`) REFERENCES `room` (`id`),
+  CONSTRAINT `roomdetailplayer_userid_foreign` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roomparticipant`
+--
+
+LOCK TABLES `roomdetailplayer` WRITE;
+/*!40000 ALTER TABLE `roomdetailplayer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roomdetailplayer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
 --
 -- Table structure for table `safetysystem`
 --
