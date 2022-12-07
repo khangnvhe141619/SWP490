1ae9d8ac241b2897b4a898beab88ff5ad10cfb44
SyNV2 <Synvhe141716@fpt.edu.vn>
2022-12-07 23:31:23 +0700

    cm search news

--- car-aution-system/src/main/java/com/workshop/carauctionsystem/controller/NewsController.java
+++ car-aution-system/src/main/java/com/workshop/carauctionsystem/controller/NewsController.java
@@ -1,11 +1,17 @@
 package com.workshop.carauctionsystem.controller;
 
 import com.workshop.carauctionsystem.entity.News;
+import com.workshop.carauctionsystem.model.NewsModel;
 import com.workshop.carauctionsystem.service.NewsService;
+import com.workshop.carauctionsystem.service.impl.NewsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,28 +23,46 @@ public class NewsController {
     NewsService newsService1;
 
 
+
+
     @GetMapping("/user/news")
-    public ModelAndView redirect() {
-        return listNews(1);
-    }
-
-    @GetMapping("/user/news/{pageNo1}")
-    public ModelAndView listNews(@PathVariable(value = "pageNo1") int pageNo) {
-        ModelAndView view = new ModelAndView();
-        int pageSize = 3;
-        Page<News> page = newsService1.findPaginated(pageNo, pageSize);
-
+    public ModelAndView showList(@RequestParam(defaultValue = "1") int page,
+                                 @RequestParam(defaultValue = "id") String id,
+                                 @RequestParam(defaultValue = "") String search,
+                                 Model model) {
+        ModelAndView modelAndView = null;
         Page<News> top5 = newsService1.getTop5(1);
         List<News> top = top5.getContent();
+        Page<News> list = newsService1.findAllNewsOrderById(PageRequest.of(page-1, 5, Sort.by(id)), search);
+        if (!list.isEmpty()) {
 
-        List<News> listNews = page.getContent();
-        view.addObject("pageNo", pageNo);
-        view.addObject("total", page.getTotalPages());
-        view.addObject("list", listNews);
-        view.addObject("top5", top);
-        view.setViewName("blog");
-        return view;
+            modelAndView = new ModelAndView("blog");
+            modelAndView.addObject("top5", top);
+            modelAndView.addObject("page", page);
+            modelAndView.addObject("total", list.getTotalPages());
+            modelAndView.addObject("list", list);
+        } else {
+            modelAndView = new ModelAndView("page404");
+        }
+        return modelAndView;
     }
+//    @GetMapping("/user/news/{pageNo1}")
+//    public ModelAndView listNews(@PathVariable(value = "pageNo1") int pageNo) {
+//        ModelAndView view = new ModelAndView();
+//        int pageSize = 5;
+//        Page<News> page = newsService1.findPaginated(pageNo, pageSize);
+//
+//        Page<News> top5 = newsService1.getTop5(1);
+//        List<News> top = top5.getContent();
+//
+//        List<News> listNews = page.getContent();
+//        view.addObject("pageNo", pageNo);
+//        view.addObject("total", page.getTotalPages());
+//        view.addObject("list", listNews);
+//        view.addObject("top5", top);
+//        view.setViewName("blog");
+//        return view;
+//    }
 
     @GetMapping("/user/details/{id}")
     public ModelAndView newsDetails(@PathVariable(value = "id") long id) {
